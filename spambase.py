8197460f3a5b7477476fbf001049a1742ef4577f
Aman Bharadwaj <amanzoot@gmail.com>
2018-01-11 11:32:33 +0530

    Update spambase.py

--- spambase.py
+++ spambase.py
@@ -7,59 +7,23 @@ import pandas as pd
 from scipy import ndimage
 from spamutil import *
 def L_layer_model(X, Y, layers_dims, learning_rate = 0.009, num_iterations = 100, print_cost=False):#lr was 0.009
-    """
-    Implements a L-layer neural network: [LINEAR->RELU]*(L-1)->LINEAR->SIGMOID.
-
-    Arguments:
-    X -- data, numpy array of shape (number of examples, num_px * num_px * 3)
-    Y -- true "label" vector (containing 0 if cat, 1 if non-cat), of shape (1, number of examples)
-    layers_dims -- list containing the input size and each layer size, of length (number of layers + 1).
-    learning_rate -- learning rate of the gradient descent update rule
-    num_iterations -- number of iterations of the optimization loop
-    print_cost -- if True, it prints the cost every 100 steps
-
-    Returns:
-    parameters -- parameters learnt by the mode
-l. They can then be used to predict.
-    """
-
+  
     np.random.seed(1)
-    costs = []                         # keep track of cost
-
-    # Parameters initialization.
-    ### START CODE HERE ###
+    costs = []            
     parameters = initialize_parameters_deep(layers_dims)
-    ### END CODE HERE ###
 
-    # Loop (gradient descent)
     for i in range(0, num_iterations):
-
-        # Forward propagation: [LINEAR -> RELU]*(L-1) -> LINEAR -> SIGMOID.
-        ### START CODE HERE ### (≈ 1 line of code)
         AL, caches = L_model_forward(X, parameters)
-        ### END CODE HERE ###
-
-        # Compute cost.
-        ### START CODE HERE ### (≈ 1 line of code)
         cost = compute_cost(AL, Y)
-        ### END CODE HERE ###
-
-        # Backward propagation.
-        ### START CODE HERE ### (≈ 1 line of code)
         grads = L_model_backward(AL, Y, caches)
-        ### END CODE HERE ###
 
-        # Update parameters.
-        ### START CODE HERE ### (≈ 1 line of code)
         parameters = update_parameters(parameters, grads, learning_rate)
-        ### END CODE HERE ###
-        # Print the cost every 100 training example
+
         if print_cost and i % 100 == 0:
             print ("Cost after iteration %i: %f" %(i, cost))
         if print_cost and i % 100 == 0:
             costs.append(cost)
 
-    # plot the cost
     plt.plot(np.squeeze(costs))
     plt.ylabel('cost')
     plt.xlabel('iterations (per tens)')
@@ -68,13 +32,9 @@ l. They can then be used to predict.
 
     return parameters
 
-#%matplotlib inline
 plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
 plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
-
-#%load_ext autoreload
-#%autoreload 2
 
 np.random.seed(1)
 train_x_orig, train_y, test_x_orig, test_y, classes = load_data()
@@ -84,7 +44,6 @@ m_test = test_x_orig.shape[0]
 train_x_flatten = train_x_orig.reshape(train_x_orig.shape[0], -1).T   # The "-1" makes reshape flatten the remaining dimensions
 test_x_flatten = test_x_orig.reshape(test_x_orig.shape[0], -1).T
 
-# Standardize data to have feature values between 0 and 1.
 train_x = train_x_flatten
 test_x = test_x_flatten
 layers_dims = [57, 20, 7, 5, 5, 1] #  5-layer model
